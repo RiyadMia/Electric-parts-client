e5e1b1284ec0abfbeb259a3cfb3b7ebf22e488ab
RiyadMia <96831477+RiyadMia@users.noreply.github.com>
2022-05-23 15:08:30 +0600

    make admin

--- src/components/Deashbord/UserRow.js
+++ src/components/Deashbord/UserRow.js
@@ -1,13 +1,39 @@
 import React from "react";
+import { toast } from "react-toastify";
 
 const UserRow = ({ user, refetch }) => {
   const { email, role } = user;
+  const makeAdmin = () => {
+    fetch(`http://localhost:5000/user/admin/${email}`, {
+      method: "PUT",
+      headers: {
+        authorization: `Beare ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => {
+        if (res.status === 403) {
+          toast.error("field to make to admin");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          refetch();
+          console.log(data);
+          toast.success("Successfuly made an admin");
+        }
+      });
+  };
   return (
     <tr>
       <th>1</th>
       <td>{email}</td>
       <td>
-        {role !== "admin" && <button class="btn btn-xs">Make Admin</button>}
+        {role !== "admin" && (
+          <button onClick={makeAdmin} class="btn btn-xs">
+            Make Admin
+          </button>
+        )}
       </td>
       <td>
         <button class="btn btn-xs">Remove User</button>
